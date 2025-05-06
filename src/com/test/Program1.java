e669681a182136f65aa71890351a8440a4db6e6e
Shwetha K <shwethak748@gmail.com>
2025-05-06 09:48:25 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -3,17 +3,11 @@ package com.test;
 public class Program1 {
 
 	public static void main(String[] args) { 
-		Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        double number = scanner.nextDouble();
-
-        double square = number * number; 
-
-        System.out.println("The square of " + number + " is: " + square);
+	     Scanner scanner = new Scanner(System.in);
+	     System.out.print("Enter a number: ");
+	     double number = scanner.nextDouble();
+	     double square = number * number; 
+	     System.out.println("The square of " + number + " is: " + square);
     }
 }
 
-	}
-
-}
