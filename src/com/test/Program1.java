a3ce156b67dc4448d8cc959fee053fb052383c5f
Shwetha K <shwethak748@gmail.com>
2025-05-06 09:33:03 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -2,8 +2,17 @@ package com.test;
 
 public class Program1 {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	public static void main(String[] args) { 
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        double square = number * number; // or Math.pow(number, 2)
+
+        System.out.println("The square of " + number + " is: " + square);
+    }
+}
 
 	}
 
